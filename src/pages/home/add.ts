d4d5488835272ac0a534bade0aa177596833a086
rbrandonc <r.brandon.c@gmail.com>
2017-09-13 13:11:28 -0400

    v1

--- src/pages/home/add.ts
+++ src/pages/home/add.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, Events, NavParams  } from 'ionic-angular';
+import { NavController, Events, NavParams, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'add-weight',
@@ -7,6 +7,7 @@ import { NavController, Events, NavParams  } from 'ionic-angular';
 })
 export class AddWeight {
   plates;
+  barWeight;
 
   imperial = [
     ".5", "1", "2.5", "5", "10", "15", "25", "35", "45"
@@ -19,11 +20,61 @@ export class AddWeight {
   standard: string = "lbs";
 
   constructor(public navCtrl: NavController, public event: Events,
-    public navParams: NavParams) {
+    public navParams: NavParams, public alert: AlertController) {
       this.plates = navParams.get('plates');
       this.standard = navParams.get('standard');
+      this.barWeight = navParams.get('barWeight');
       console.log(this.standard);
       console.log(this.plates);
+  }
+
+  changeBar() {
+    console.log(this.barWeight);
+    let alert = this.alert.create({
+      title: 'Change Bar Weight',
+      inputs: [
+        {
+          label: '45lbs',
+          value: 'lbs22.5',
+          type: "radio",
+          checked: this.barWeight === "lbs22.5"
+        },
+        {
+          label: '25lbs',
+          value: 'lbs12.5',
+          type: "radio",
+          checked: this.barWeight === "lbs12.5"
+        },
+        {
+          label: '20kgs',
+          value: 'kgs10',
+          type: "radio",
+          checked: this.barWeight === "kgs10"
+        },
+        {
+          label: '15kgs',
+          value: 'kgs7.5',
+          type: "radio",
+          checked: this.barWeight === "kgs7.5"
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Confirm',
+          handler: data => {
+            this.event.publish("changeBar", data);
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
   add(item) {
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,32 +11,58 @@ export class HomePage {
   total: number = 0;
   standard: string = "lbs"
   formattedTotal: string;
+  barWeight = "lbs22.5";
+  firstLaunch = "false";
   plates;
 
   constructor(public navCtrl: NavController, public event: Events) {
-    this.event.subscribe('addWeight', (item: string) => {
-      this.plates[item]++;
-      this.total += item.substring(0, 3) === this.standard || item.substring(0, 3) === this.standard + 's' ?
-        2*parseFloat(item.substring(3, item.length)) :
-        2*this.convert(item.substring(0, 3), parseFloat(item.substring(3, item.length)));
-        this.formatTotal();
-    })
+    this.event.subscribe('addWeight', (item) => this.addPlate(item));
 
-    this.event.subscribe('removeWeight', (item) => {
-      console.log(this.plates[item], item);
-      if(this.plates[item] > 0){
-        console.log('asdf');
-        this.plates[item]--;
-        this.total -= item.substring(0, 3) === this.standard || item.substring(0, 3) === this.standard + 's' ?
-          2*parseFloat(item.substring(3, item.length)) :
-          2*this.convert(item.substring(0, 3), parseFloat(item.substring(3, item.length)));
-        this.formatTotal();
-      }
+    this.event.subscribe('removeWeight', (item) => this.removePlate(item));
+
+    this.event.subscribe('changeBar', (weight) => {
+      this.removeWeight(this.barWeight);
+      this.addWeight(weight);
+      this.barWeight = weight;
     })
 
     this.event.subscribe('resetWeight', () => {
       this.resetWeight();
     })
+    console.log(window.localStorage.getItem('firstlaunch'))
+    this.firstLaunch = window.localStorage.getItem('firstlaunch') || 'true';
+    console.log(this.firstLaunch);
+  }
+
+  closeTips() {
+    this.firstLaunch = 'false';
+    window.localStorage.setItem('firstlaunch', 'false');
+  }
+
+  addPlate(item: string) {
+    this.addWeight(item);
+    this.plates[item]++;
+  }
+
+  removePlate(item: string) {
+    if(this.plates[item] > 0){
+      this.removeWeight(item);
+      this.plates[item]--;
+    }
+  }
+
+  addWeight(item: string) {
+    this.total += item.substring(0, 3) === this.standard || item.substring(0, 3) === this.standard + 's' ?
+      2*parseFloat(item.substring(3, item.length)) :
+      2*this.convert(item.substring(0, 3), parseFloat(item.substring(3, item.length)));
+      this.formatTotal();
+  }
+
+  removeWeight (item) {
+    this.total -= item.substring(0, 3) === this.standard || item.substring(0, 3) === this.standard + 's' ?
+      2*parseFloat(item.substring(3, item.length)) :
+      2*this.convert(item.substring(0, 3), parseFloat(item.substring(3, item.length)));
+    this.formatTotal();
   }
 
   ngOnInit() {
@@ -46,6 +72,34 @@ export class HomePage {
   formatTotal() {
     this.formattedTotal = this.total.toString() === this.total.toString().substring(0, this.total.toString().indexOf('.'))
     ? this.total.toString() : this.total.toFixed(2);
+  }
+
+  getPlates() {
+    var sorted = Object.keys(this.plates).sort((a, b) => {
+      var weighta = a.substring(0,3) == "lbs" ? parseFloat(a.substring(3, a.length)) : this.convert("kgs", parseFloat(a.substring(3, a.length)));
+      var weightb = b.substring(0,3) == "lbs" ? parseFloat(b.substring(3, b.length)) : this.convert("kgs", parseFloat(b.substring(3, b.length)));
+      if(weighta > weightb) return 1;
+      if(weighta < weightb) return -1;
+      return 0;
+    });
+    console.log(sorted)
+    return sorted;
+  }
+
+  getNumPlates(plate) {
+    console.log(plate, this.plates[plate])
+    var temp = Array(this.plates[plate]).fill(0);
+    // console.log(temp);
+    return temp;
+  }
+
+  plateSize(a) {
+    var weight =  a.substring(0,3) == "lbs" ? parseFloat(a.substring(3, a.length)) : this.convert("kgs", parseFloat(a.substring(3, a.length)));
+    // console.log(weight);plateExtraSmall
+    if(weight > 0  && weight <= 15) return "plateExtraSmall";
+    if(weight > 15  && weight <= 25) return "plateSmall";
+    if(weight > 25 && weight <= 35) return "plateMedium";
+    if(weight > 35) return "plateLarge";
   }
 
   resetWeight(){
@@ -70,26 +124,27 @@ export class HomePage {
         kgs25: 0
       }
     } else {
-      console.log(this.plates);
+      // console.log(this.plates);
       for(let i in this.plates) {
         this.plates[i] = 0;
       }
-      console.log(this.plates);
+      // console.log(this.plates);
     }
+    this.addWeight(this.barWeight);
   }
 
   add() {
-    this.navCtrl.push(AddWeight, {plates: this.plates, standard: this.standard});
+    this.navCtrl.push(AddWeight, {plates: this.plates, standard: this.standard, barWeight: this.barWeight});
   }
 
   convert(from, weight) {
-    console.log(from, weight);
+    // console.log(from, weight);
     if(from === 'lbs') {
-      console.log('convert from lbs')
+      // console.log('convert from lbs')
       return weight/2.2046;
     }
     if(from === 'kgs' || from === 'kg') {
-      console.log('convert from kg')
+      // console.log('convert from kg')
       return weight*2.2046;
     }
   }
@@ -103,13 +158,4 @@ export class HomePage {
       this.standard = 'lbs'
     }
   }
-
-  getWeight() {
-
-  }
-
-  getPlates() {
-
-  }
-
 }
